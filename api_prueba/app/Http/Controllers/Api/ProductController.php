6be2b99184819ff982f89d65e3e196ab41255776
menssajero <willkergaciab@gmail.com>
2023-07-21 16:06:14 -0400

    cosas

---
+++ api_prueba/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+
+
+class ProductController extends Controller
+{
+    public function store(Request $request)
+    {
+       $request->validate([
+        'nombre' => ['required','string','min:3'],
+        'descripcion' => ['required','string','min:3'],
+        'tipo' => ['required','string','min:5'],
+        'stock' => ['required','integer','min:1'],
+       ]); 
+       return response()->json($request);
+
+       DB::beginTransaction();
+       try {
+         $product = Product::create([
+            'nombre'=> $request -> nombre,
+            'descripcion'=> $request -> descripcion,
+            'tipo'=> $request -> tipo,
+            'stock'=> $request -> stock,
+         ]);
+         DB::commit();
+       } catch (\Throwable $th) {
+            DB::rollBack();
+            return response()->json('error al mandar datos');
+       }
+       return response()->json('datos insertados');
+    }
+}
---
+++ api_prueba/app/Models/Product.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $table= 'tu_tabla';
+
+    protected $primaryKey = 'id';
+
+    protected $fillable = ['nombre', 'descripcion', 'tipo', 'stock'
+];
+}
